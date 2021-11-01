a406f9425f59609031581a335b064dcbc02a9c58
Lee Shaw <ldeshaw6@aol.com>
2021-11-01 19:10:29 +0000

    all pages added

--- main.py
+++ main.py
@@ -33,6 +33,65 @@ def ch1C():
 def e1C():
     return render_template('ending1C.html'), 200
 
+@app.route('/choice2A') 
+def ch2A():
+    return render_template('choice2A.html'), 200
+
+@app.route('/ending2A')
+def e2A():
+    return render_template('ending2A.html'), 200
+
+@app.route('/choice2B')
+def ch2B():
+    return render_template('choice2B.html'), 200
+
+@app.route('/ending2B') 
+def e2B():
+    return render_template('ending2B.html'), 200
+
+@app.route('/choice2C')
+def ch2C():
+    return render_template('choice2C.html'), 200 
+
+@app.route('/ending2C')
+def e2C(): 
+    return render_template('ending2C.html'), 200
+
+@app.route('/choice3A')
+def ch3A():
+    return render_template('choice3A.html'), 200
+
+@app.route('/choice3B')
+def ch3B():
+    return render_template('choice3B.html'), 200
+
+@app.route('/choice3C')
+def ch3C():
+    return render_template('choice3C.html'), 200
+
+@app.route('/choice4A')
+def ch4A():
+    return render_template('choice4A.html'), 200
+
+@app.route('/choice4B')
+def ch4B():
+    return render_template('choice4B.html'), 200
+
+@app.route('/ending3A')
+def e3A(): 
+    return render_template('ending3A.html'), 200
+
+@app.route('/ending3B')
+def e3B():
+    return render_template('ending3B.html'), 200
+
+@app.route('/ending4A')
+def e4A():
+    return render_template('ending4A.html'), 200
+
+@app.route('/ending4B')
+def e4B():
+    return render_template('ending4B.html'), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
